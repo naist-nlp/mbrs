713d84abefc201594f9a0901de26ea9d100fcabc
Hiroyuki Deguchi <deguchi.hiroyuki.db0@is.naist.jp>
2024-07-26 16:04:32 +0900

    Implement memory efficient compute_transition_scores

--- mbrs/cli/generate.py
+++ mbrs/cli/generate.py
@@ -14,7 +14,7 @@ from transformers import (
     M2M100ForConditionalGeneration,
     set_seed,
 )
-from transformers.generation.utils import GenerateOutput
+from transformers.generation.utils import GenerateOutput, GenerationMixin
 
 from mbrs import timer
 
@@ -101,6 +101,130 @@ class Sample:
     length_normalized_lprob: Optional[float] = None
 
 
+def memory_efficient_compute_transition_scores(
+    model: GenerationMixin,
+    sequences: torch.Tensor,
+    scores: tuple[torch.Tensor, ...],
+    beam_indices: Optional[torch.Tensor] = None,
+    normalize_logits: bool = False,
+) -> torch.Tensor:
+    """
+    Computes the transition scores of sequences given the generation scores (and beam indices, if beam search was
+    used). This is a convenient method to quicky obtain the scores of the selected tokens at generation time.
+
+    This is another implementation for memory efficiency.
+
+    Parameters:
+        model (`GenerationMixin`): Generation model.
+        sequences (`torch.LongTensor`):
+            The generated sequences. The second dimension (sequence_length) is either equal to `max_length` or
+            shorter if all batches finished early due to the `eos_token_id`.
+        scores (`tuple(torch.FloatTensor)`):
+            Transition scores for each vocabulary token at each generation step. Beam transition scores consisting
+            of log probabilities of tokens conditioned on log softmax of previously generated tokens in this beam.
+            Tuple of `torch.FloatTensor` with up to `max_new_tokens` elements (one element for each generated token),
+            with each tensor of shape `(batch_size*num_beams, config.vocab_size)`.
+        beam_indices (`torch.LongTensor`, *optional*):
+            Beam indices of generated token id at each generation step. `torch.LongTensor` of shape
+            `(batch_size*num_return_sequences, sequence_length)`. Only required if a `num_beams>1` at
+            generate-time.
+        normalize_logits (`bool`, *optional*, defaults to `False`):
+            Whether to normalize the logits (which, for legacy reasons, may be unnormalized).
+
+    Return:
+        `torch.Tensor`: A `torch.Tensor` of shape `(batch_size*num_return_sequences, sequence_length)` containing
+            the transition scores (logits)
+
+    Examples:
+
+    ```python
+    >>> from transformers import GPT2Tokenizer, AutoModelForCausalLM
+    >>> import numpy as np
+
+    >>> tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    >>> model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
+    >>> tokenizer.pad_token_id = tokenizer.eos_token_id
+    >>> inputs = tokenizer(["Today is"], return_tensors="pt")
+
+    >>> # Example 1: Print the scores for each token generated with Greedy Search
+    >>> outputs = model.generate(**inputs, max_new_tokens=5, return_dict_in_generate=True, output_scores=True)
+    >>> transition_scores = memory_efficient_compute_transition_scores(
+    ...     model, outputs.sequences, outputs.scores, normalize_logits=True
+    ... )
+    >>> # input_length is the length of the input prompt for decoder-only models, like the GPT family, and 1 for
+    >>> # encoder-decoder models, like BART or T5.
+    >>> input_length = 1 if model.config.is_encoder_decoder else inputs.input_ids.shape[1]
+    >>> generated_tokens = outputs.sequences[:, input_length:]
+    >>> for tok, score in zip(generated_tokens[0], transition_scores[0]):
+    ...     # | token | token string | log probability | probability
+    ...     print(f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
+    |   262 |  the     | -1.414 | 24.33%
+    |  1110 |  day     | -2.609 | 7.36%
+    |   618 |  when    | -2.010 | 13.40%
+    |   356 |  we      | -1.859 | 15.58%
+    |   460 |  can     | -2.508 | 8.14%
+
+    >>> # Example 2: Reconstruct the sequence scores from Beam Search
+    >>> outputs = model.generate(
+    ...     **inputs,
+    ...     max_new_tokens=5,
+    ...     num_beams=4,
+    ...     num_return_sequences=4,
+    ...     return_dict_in_generate=True,
+    ...     output_scores=True,
+    ... )
+    >>> transition_scores = memory_efficient_compute_transition_scores(
+    ...     model, outputs.sequences, outputs.scores, outputs.beam_indices, normalize_logits=False
+    ... )
+    >>> # If you sum the generated tokens' scores and apply the length penalty, you'll get the sequence scores.
+    >>> # Tip 1: recomputing the scores is only guaranteed to match with `normalize_logits=False`. Depending on the
+    >>> # use case, you might want to recompute it with `normalize_logits=True`.
+    >>> # Tip 2: the output length does NOT include the input length
+    >>> output_length = np.sum(transition_scores.numpy() < 0, axis=1)
+    >>> length_penalty = model.generation_config.length_penalty
+    >>> reconstructed_scores = transition_scores.sum(axis=1) / (output_length**length_penalty)
+    >>> print(np.allclose(outputs.sequences_scores, reconstructed_scores))
+    True
+    ```"""
+    # 1. In absence of `beam_indices`, we can assume that we come from e.g. greedy search, which is equivalent
+    # to a beam search approach were the first (and only) beam is always selected
+    if beam_indices is None:
+        beam_indices = torch.arange(scores[0].shape[0]).view(-1, 1).to(sequences.device)
+        beam_indices = beam_indices.expand(-1, len(scores))
+
+    # 2. Optionally normalize the logits (across the vocab dimension)
+    if normalize_logits:
+        scores = tuple(s.log_softmax(dim=1) for s in scores)
+
+    # 3. cut beam_indices to longest beam length
+    beam_indices_mask = beam_indices < 0
+    max_beam_length = (1 - beam_indices_mask.long()).sum(-1).max()
+    beam_indices = beam_indices.clone()[:, :max_beam_length]
+    beam_indices_mask = beam_indices_mask[:, :max_beam_length]
+
+    # 4. Set indices of beams that finished early to 0; such indices will be masked correctly afterwards
+    beam_indices[beam_indices_mask] = 0
+
+    # 5. multiply beam_indices with vocab size to gather correctly from scores
+    beam_sequence_indices = beam_indices * model.config.vocab_size
+
+    # 6. Define which indices contributed to scores
+    cut_idx = sequences.shape[-1] - max_beam_length
+    indices = sequences[:, cut_idx:] + beam_sequence_indices
+
+    # 7. Compute scores
+    transition_scores = torch.zeros(
+        indices.size(), dtype=torch.float32, device=scores[0].device
+    )
+    for step, step_scores in enumerate(scores):
+        transition_scores[:, step] = step_scores.view(-1).gather(0, indices[:, step])
+
+    # 8. Mask out transition_scores of beams that stopped early
+    transition_scores[beam_indices_mask] = 0
+
+    return transition_scores
+
+
 def main(args: Namespace) -> None:
     set_seed(args.seed)
 
@@ -163,20 +287,27 @@ def main(args: Namespace) -> None:
             ]
 
         sequences = model_outputs.sequences
-        scores = tuple([s.log_softmax(dim=-1) for s in model_outputs.scores])
+        scores = model_outputs.scores
         max_length = len(scores)
         if hasattr(tokenizer, "pad_token_id"):
             pad_token_id = tokenizer.pad_token_id
             for s in scores:
+                s[:, pad_token_id] = float("-inf")
+            scores = tuple(s.log_softmax(dim=1) for s in scores)
+            for s in scores:
                 s[:, pad_token_id] = 0.0
             sequence_lengths = max_length - (sequences.eq(pad_token_id)).sum(dim=-1)
         else:
+            scores = tuple(s.log_softmax(dim=1) for s in scores)
             sequence_lengths = torch.full(
                 (sequences.size(0),), fill_value=max_length, device=sequences.device
             )
 
-        transition_scores = model.compute_transition_scores(
-            sequences, scores, beam_indices=model_outputs.get("beam_indices", None)
+        transition_scores = memory_efficient_compute_transition_scores(
+            model,
+            sequences,
+            scores,
+            beam_indices=model_outputs.get("beam_indices", None),
         )
         lprobs = transition_scores.sum(dim=-1)
         length_normalized_lprobs = lprobs / (sequence_lengths**length_penalty)
